db22b88efee436e49a8c090ce387226b482ee044
Ketan_Pipania <ketanpipania@gmail.com>
2026-06-15 12:56:17 +0530

    Add variables example

--- basics/Variables.java
+++ basics/Variables.java
@@ -1,1 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 20;
+        double salary = 50000.50;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Rahul";
 
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
