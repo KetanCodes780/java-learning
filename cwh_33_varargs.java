8b4ead26732fafc2c71923cd5aea194e5f31b860
Ketan_Pipania <ketanpipania@gmail.com>
2026-07-21 21:32:17 +0530

    Update cwh_33_varargs.java

--- cwh_33_varargs.java
+++ cwh_33_varargs.java
@@ -11,8 +11,7 @@ public class cwh_33_varargs {
     // }
     // Varags->Varargs (Variable Arguments) in Java allow a method to 
     // accept any number of arguments of the same type.
-    // static int sum(int x,int..arr)-> here the x argument is
-    compulsory 
+    // static int sum(int x,int..arr)-> here the x argument is compulsory.
     static int sum(int... arr){
         // Available as int []arr;
         int result=0;
