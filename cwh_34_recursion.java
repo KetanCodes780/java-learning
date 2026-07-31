df7607450d8cf048a3efc9d587a60d590509cdf4
Ketan_Pipania <ketanpipania@gmail.com>
2026-07-31 22:11:12 +0530

    Update cwh_34_recursion.java

--- cwh_34_recursion.java
+++ cwh_34_recursion.java
@@ -1,9 +1,9 @@
 
 public class cwh_34_recursion {
-    // factorial(0) = 1
-    // factorial(n) = n * n-1 *....1
-    // factorial(5) = 5 * 4 * 3 * 2 * 1 = 120
-    // factorial(n) = n * factorial(n-1)
+    // factorial(0) = 1.
+    // factorial(n) = n * n-1 *....1.
+    // factorial(5) = 5 * 4 * 3 * 2 * 1 = 120.
+    // factorial(n) = n * factorial(n-1).
 
     static int factorial(int n){
         if(n==0 || n==1){
